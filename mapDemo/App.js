0a3ee22201be2a95a57c0ab0393a638400b52b57
BlablaTara <tara0561@stud.kea.dk>
2025-05-30 16:17:37 +0200

    nu med lokation

--- mapDemo/App.js
+++ mapDemo/App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, View } from "react-native";
 import MapView, { Marker, marker } from "react-native-maps";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import * as Location from 'expo-location';
 
 //skriv npx expo install react-native-maps
 // brug MapView - og du har maps på.
@@ -21,6 +22,43 @@ export default function App() {
     longitudeDelta: 20, // Hvor zoomede ind ud skal vi være?
   });
 
+  // denne har en variabel, som "ikke forsvinder" 
+  // skaber ikke en ny rendering, som useState gør.
+  const mapView = useRef(null); // ref til mmapView objekt.
+  const locationSubscribtion = useRef(null) // når vi lukker appen, så lytter vi ikke mere.
+
+  useEffect(() => {
+    async function startListening() {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted') {
+        alert("Ingen adgang til lokationen")
+        return
+      }
+      locationSubscribtion.current = await Location.watchPositionAsync({
+        distanceInterval: 100,
+        accuracy: Location.Accuracy.High
+      }, (location) => {
+        const newRegion = {
+          latitude: location.coords.latitude, 
+          longitude: location.coords.longitude, 
+          latitudeDelta: 20, 
+          longitudeDelta: 20,
+
+        }
+        setRegion(newRegion) //flytter kortet til den nye lokation
+        if (mapView.current) {
+          mapView.current.animateToRegion(newRegion)
+        }
+    })
+    }
+    startListening()
+    return () => {
+      if (locationSubscribtion.current) {
+        locationSubscribtion.current.remove()
+      }
+    }
+  }, []) // med tom array, betyder det at den kun skal køre en enkel gang. 
+
   function addMarker(data) {
     const { latitude, longitude } = data.nativeEvent.coordinate;
     const newMarker = {
@@ -33,7 +71,7 @@ export default function App() {
 
 
   // funktion, der viser noget når man trykker på ens pin. 
-  
+
   function onMarkerPress(text) {
     alert("You pressed " + text)
   }
